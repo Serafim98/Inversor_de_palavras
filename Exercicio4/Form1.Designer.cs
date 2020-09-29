98786fe55aea94d1af18851286ff52d9937997ba
LUIS SERAFIM <fernandodss30@hotmail.com>
2020-09-29 15:09:56 -0300

    Implementação simples concluida.

---
+++ Exercicio4/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Exercicio4 {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TB_original = new System.Windows.Forms.TextBox();
+            this.BT_Processa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TB_Invertida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frase original";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TB_original
+            // 
+            this.TB_original.Location = new System.Drawing.Point(106, 29);
+            this.TB_original.Name = "TB_original";
+            this.TB_original.Size = new System.Drawing.Size(100, 20);
+            this.TB_original.TabIndex = 1;
+            // 
+            // BT_Processa
+            // 
+            this.BT_Processa.Location = new System.Drawing.Point(258, 29);
+            this.BT_Processa.Name = "BT_Processa";
+            this.BT_Processa.Size = new System.Drawing.Size(75, 23);
+            this.BT_Processa.TabIndex = 2;
+            this.BT_Processa.Text = "Processa";
+            this.BT_Processa.UseVisualStyleBackColor = true;
+            this.BT_Processa.Click += new System.EventHandler(this.BT_Processa_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Frase invertida";
+            // 
+            // TB_Invertida
+            // 
+            this.TB_Invertida.Location = new System.Drawing.Point(106, 66);
+            this.TB_Invertida.Name = "TB_Invertida";
+            this.TB_Invertida.Size = new System.Drawing.Size(100, 20);
+            this.TB_Invertida.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 196);
+            this.Controls.Add(this.TB_Invertida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BT_Processa);
+            this.Controls.Add(this.TB_original);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TB_original;
+        private System.Windows.Forms.Button BT_Processa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TB_Invertida;
+    }
+}
+
---
+++ Exercicio4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio4 {
+    public partial class Form1 : Form {
+
+        const int MAX = 50;
+        struct tp_pilha {
+            public int topo;
+            public char[] vetor;
+        }
+        tp_pilha pilha;
+
+        // Inicialização
+        void Inicializa(ref tp_pilha p) {
+            p.topo = -1;
+            p.vetor = new char[MAX];
+        }
+
+        bool EstaVazia(tp_pilha p) {
+            return (p.topo == -1);
+        }
+
+        bool EstaCheia(tp_pilha p) {
+            return (p.topo == MAX - 1);
+        }
+
+        void Insere(ref tp_pilha p, char valor) {
+            p.vetor[++p.topo] = valor;
+        }
+
+        char Remove(ref tp_pilha p) {
+            return (p.vetor[p.topo--]);
+        }
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void BT_Processa_Click(object sender, EventArgs e) {
+            Inicializa(ref pilha);
+            String P_original = TB_original.Text;
+            String Resultado = "";
+            int i, j;
+            i = 0;
+            char c;
+            while (i < P_original.Length) {
+                while (i < P_original.Length && P_original[i] != ' ') {
+                    c = P_original[i];
+                    Insere(ref pilha, c);
+                    i++;
+                }
+                j = 0;
+                while (pilha.topo != -1) {
+                    c = Remove(ref pilha);
+                    Resultado = Resultado + c;
+                    j++;
+                }
+                i++;
+                Resultado = Resultado + " ";
+            }
+            TB_Invertida.Text = Resultado;
+        }
+    }
+}
